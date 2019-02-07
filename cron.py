63f13904b65e36689a61333741b65f85ae1045cb
farhan5900 <farhan5900@gmail.com>
2019-02-07 19:47:20 +0530

    Added checking for length of argument and job existance in crontab

--- cron.py
+++ cron.py
@@ -1,12 +1,28 @@
 import time
 from crontab import CronTab
 import sys
+import os
 
-uname = sys.argv[1]
+def main():
+  cron_tabs = []
 
-users_cron = CronTab(user=uname)
-job = users_cron[1]
+  # check whether username is provided or not as an argument
+  # if not current user is considered
+  if len(sys.argv) < 2:
+    cron_tabs = CronTab(user=True)
+  else:
+    cron_tabs = CronTab(sys.argv[1])
 
-while True:
-  job.run_pending()
-  time.sleep(1)
+  # check if any crontab is defined, if it is considered only
+  # last one
+  if len(cron_tabs) > 1:
+    job = cron_tabs[-1]
+  else:
+    sys.exit(1) # no cron job is defined for the given user
+
+  while True:
+    job.run_pending()
+    time.sleep(1)
+
+if __name__ == '__main__':
+  main()
