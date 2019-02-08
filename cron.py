36ec573e14a042cd72e5966d9736d2dd60610c5f
farhan5900 <farhan5900@gmail.com>
2019-02-08 09:50:51 +0530

    Added logic to run multiple jobs

--- cron.py
+++ cron.py
@@ -1,27 +1,19 @@
 import time
 from crontab import CronTab
 import sys
-import os
 
 def main():
-  cron_tabs = []
-
   # check whether username is provided or not as an argument
   # if not current user is considered
-  if len(sys.argv) < 2:
-    cron_tabs = CronTab(user=True)
-  else:
-    cron_tabs = CronTab(sys.argv[1])
+  username = True if len(sys.argv) < 2 else sys.argv[1]
 
-  # check if any crontab is defined, if it is considered only
-  # last one
-  if len(cron_tabs) > 1:
-    job = cron_tabs[-1]
-  else:
-    sys.exit(1) # no cron job is defined for the given user
+  # read all the crontabs from /var/spool/cron/crontabs/$username
+  cron_tabs = CronTab(user=username)
 
   while True:
-    job.run_pending()
+    # check for all the pending jobs and schedule it to run
+    for job in cron_tabs:
+      job.run_pending()
     time.sleep(1)
 
 if __name__ == '__main__':
